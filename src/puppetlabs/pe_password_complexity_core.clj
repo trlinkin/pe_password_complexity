b6f883b8904ee56a25e9d06591a4bf3eba9222f4
Thomas Linkin <trlinkin@gmail.com>
2020-09-04 10:37:38 -0400

    Creating the basic formatting for the service
    This commit removes what we should not need from the base of the
    template. This commit also starts to lay out what should be the frame
    for the clojure in the module.

--- src/puppetlabs/pe_password_complexity_core.clj
+++ src/puppetlabs/pe_password_complexity_core.clj
@@ -1,6 +1,24 @@
 (ns puppetlabs.pe-password-complexity-core)
 
-(defn hello
-  "Say hello to caller"
-  [caller]
-  (format "Hello, %s!" caller))
+(defn retain-old-fn
+  [fn-name old-fn]
+  (def fn-name old-fn))
+
+(defn new-create-user!
+  "creates use while validating password"
+  [db user]
+  ())
+
+(defn validate-password
+  [password]
+  ())
+
+(defn new-update-user-password!
+  [db login new-password clear-token-uuid?]
+  (puppetlabs.pe-password-complexity-core/validate-password new-password)
+  (puppetlabs.pe-password-complexity-core/old-update-user-password! db login new-password clear-token-uuid?))
+
+(defn new-create-user!
+  [db user]
+  (puppetlabs.pe-password-complexity-core/validate-password (-> user [:password]))
+  (puppetlabs.pe-password-complexity-core/old-create-user! db user))
--- src/puppetlabs/pe_password_complexity_service.clj
+++ src/puppetlabs/pe_password_complexity_service.clj
@@ -1,22 +1,11 @@
 (ns puppetlabs.pe-password-complexity-service
   (:require [clojure.tools.logging :as log]
             [puppetlabs.pe-password-complexity-core :as core]
-            [puppetlabs.trapperkeeper.core :as trapperkeeper]))
+            [puppetlabs.trapperkeeper.core :as trapperkeeper]
+            [puppetlabs.rbac.storage :as st]))
 
-(defprotocol HelloService
-  (hello [this caller]))
-
-(trapperkeeper/defservice hello-service
-  HelloService
-  []
+(trapperkeeper/defservice validation-service
+  [[:ConfigService get-in-config]]
   (init [this context]
-    (log/info "Initializing hello service")
+    (log/info "Setting up the password validation")
     context)
-  (start [this context]
-    (log/info "Starting hello service")
-    context)
-  (stop [this context]
-    (log/info "Shutting down hello service")
-    context)
-  (hello [this caller]
-         (core/hello caller)))
